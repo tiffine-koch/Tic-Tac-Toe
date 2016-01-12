1c700275e853cd410211666743494e0de9b47a2d
Apple <tiffine.koch@gmail.com>
2016-01-12 08:19:39 -0800

    Working data attribute

--- main.js
+++ main.js
@@ -20,46 +20,36 @@ function startGame() {
 
 function squareClicked() {
   var $this = $(this);
-  // if already selected, do nothing
-
-  console.log('1');
   if (whoseTurn === true) {
-    console.log('2');
-    console.log($this);
 // place marker
     var $exText = $('p:nth-of-type(1)').text();
     $this.append( $('<p>').text(x) );
 // grab data to compare to winner
-    var $dataX = $('.col-md-4[data-type]').data('type');
-    tallyX.push($dataX);
+    var dataX = $this.attr('data');
+    // console.log(dataX);
+    tallyX.push(dataX);
     $('.header').empty();
     whoseTurn = false;
     var $message2 = "Player Two's Turn";
     $('h1').append($message2).addClass('header');
-
 //check for winner
     // findWinner();
 
   } else {
-
-
-    var $ohText = $('p:nth-of-type(2)').text();
+    var $ohText = $('p:nth-of-type(3)').text();
     $this.append( $('<p>').text(o) );
 
-    console.log($this);
-    console.log('4');
-    var $dataO = $('col-md-4[data-type]').data();
-    tallyO.push($dataO);
-    whoseTurn = true;
-    console.log('test');
+    var dataO = $this.attr('data');
+    // console.log(dataO);
+    tallyO.push(dataO);
     $('.header').empty();
+    whoseTurn = true;
+
     var $message1 = "Player One's Turn";
     $('h1').append($message1).addClass('header');
     // findWinner();
     }
   }
-
-
 
 // function findWinner() {
 // var possibleWins = [[1,2,3],[3,6,9],[7,8,9],[1,4,7],[1,5,9],[2,5,8],[3,5,7],[4,5,6]]; {
@@ -70,8 +60,3 @@ function squareClicked() {
 // function resetGame() {
 //
 // }
-// var $message1 = "Player One's Turn";
-// $('h1').append($message1).addClass('header');
-//
-// var $ohText = $('p:nth-of-type(2)').text();
-// $this.append( $('<p>').text(o) );
