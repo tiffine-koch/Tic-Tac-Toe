516a4c906af334fbb600dda622440e72141cb134
Apple <tiffine.koch@gmail.com>
2016-01-11 23:17:17 -0800

    updated JS file

--- main.js
+++ main.js
@@ -2,51 +2,57 @@ $(document).ready(init);
 
 function init() {
   // var $click = $(.click);
-  // $('#start').on('click', gameControl);
-  // $('#nameButton').on('click', submitName);
-  // $('#reset').on('click', resetGame);
-  $('.square').on('click', squareClicked);
+  $('#start').on('click', startGame);
+  $('#nameButton').on('click', submitName);
+  $('#reset').on('click', resetGame);
 }
 
-// var $ohText
-// var squareSelected;
-// var $clicked;
-//
-// function gameControl() {
-//     event.preventDefault();
-//     if(!squareSelected) {
-//       playerOneTurn(event)
-//     } else if (squareSelected) {
-//       playerTwoTurn(event)
-//     }
-// }
+var $squareSelected;
+var $clicked;
+var tallyX = [];
+var tallyO = [];
 
-function squareClicked() {
-  var $message1 = "Player One's Turn";
-  $('h1').append($message1).addClass('header');
+function startGame() {
+  if ( $('.square').on('click', squareClicked) ) {
 
-//
-// function playerOneTurn() {
-  var $exText = $('p:nth-of-type(1)').text();
-  $(this).append( $('<p>').text($exText) );
-  // var $message2 = "Player Two's Turn";
-  // $('h1').append( $('<p>'.text($message2) );
-//   squareSelected = false;
+    var $message1 = "Player One's Turn";
+    $('h1').append($message1).addClass('header');
+
+    var $exText = $('p:nth-of-type(1)').text();
+    $(this).append( $('<p>').text($exText) );
+
+    var $dataX = $('.data-type').data();
+    tallyX.push($dataX);
+
+    var $message2 = "Player Two's Turn";
+    $('h1').append($message2).addClass('header');
+    console.log(test);
+    findWinner();
+
+  } else {
+
+    var $ohText = $('p:nth-of-type(2)').text();
+    $(this).append( $('<p>').text($ohText) );
+
+    var $dataO = $('.data-type').data();
+    tallyO.push($dataO);
+    console.log(test);
+    findWinner();
+
+  } else {
+    console.log(test);
+    var $message1 = "Player One's Turn";
+    $('h1').append($message1).addClass('header');
+  }
+
 }
-//
-// function playerTwoTurn() {
-//   var $ohText = $('p:nth-of-type(2)').text();
-//   $(this).append( $('<p>').text($ohText) );
-//   var $message1 = "Player One's Turn",
-//   $('h1').append( $('<p>').text($message1) );
-//   squareSelected = true;
-// }
 
+// function findWinner() {
+// var possibleWins = [[1,2,3],[3,6,9],[7,8,9],[1,4,7],[1,5,9],[2,5,8],[3,5,7]]; {
+//
+//   } var $messageWin = "You Win";
+//   $('h1').append($messageWin).addClass('header');
+// }
 // function resetGame() {
 //
-// }
-// function findWinner() {
-//   if ($('.t3').find('disc').length === 3) {
-//   alert('You win! Play again?');
-//   }
 // }
